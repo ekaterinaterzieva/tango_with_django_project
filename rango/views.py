48bb32532de3a86aeaf14d823ff409bd4c95c053
Ekaterina Terzieva <ekaterinaterzieva1@gmail.com>
2020-02-09 00:47:18 +0000

    7th chapter done

--- rango/views.py
+++ rango/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from rango.models import Category
 from rango.models import Page
 from django.http import HttpResponse
+from rango.forms import CategoryForm
+from django.shortcuts import redirect
+from django.urls import reverse
+from rango.forms import PageForm
+
 
 def index(request):
 
@@ -17,49 +22,30 @@ def index(request):
     context_dict['boldmessage'] = 'Crunchy, creamy, cookie, candy, cupcake!'
     context_dict['categories'] = category_list
     context_dict['pages'] = page_list
-    
-    # Render the response and send it back!
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     context_dict = {'MEDIA_URL': '/media/'}
     return render(request, 'rango/about.html')
 
-    #return HttpResponse("Rango says here is the about page. <br> <a href='/rango/'>Index</a>")
 def show_category(request, category_name_slug):
-	# Create a context dictionary which we can pass
-	# to the template rendering engine.
 	context_dict = {}
 
 	try:
-		# Can we find a category name slug with the given name?
-		# If we can't, the .get() method raises a DoesNotExist exception.
-		# The .get() method returns one model instance or raises an exception.
 		category = Category.objects.get(slug=category_name_slug)
 
-		# Retrieve all of the associated pages.
-		# The filter() will return a list of page objects or an empty list.
 		pages = Page.objects.filter(category=category)
 
-		# Adds our results list to the template context under name pages.
 		context_dict['pages'] = pages
-		# We also add the category object from
-		# the database to the context dictionary.
-		# We'll use this in the template to verify that the category exists.
 		context_dict['category'] = category
 	except Category.DoesNotExist:
-		# We get here if we didn't find the specified category.
-		# Don't do anything -
-		# the template will display the "no category" message for us.
 		context_dict['category'] = None
 		context_dict['pages'] = None
-
-	# Go render the response and return it to the client.
+        
 	return render(request, 'rango/category.html', context=context_dict)
 
 def show_page(request, page_name_slug):
-	# Create a context dictionary which we can pass
-	# to the template rendering engine.
 	context_dict = {}
 
 	try:
@@ -74,3 +60,45 @@ def show_page(request, page_name_slug):
 		context_dict['pages'] = None
 
 	return render(request, 'rango/category.html', context=context_dict) 
+
+def add_category(request):
+    form = CategoryForm()
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        
+        if form.is_valid():
+            form.save(commit=True)
+            return redirect('/rango/')
+            
+        else:
+            print(form.errors)
+
+    return render(request, 'rango/add_category.html', {'form': form})
+    
+def add_page(request, category_name_slug):
+    try:
+        category = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        category = None
+
+    # You cannot add a page to a Category that does not exist...
+    if category is None:
+        return redirect('/rango/')
+
+    form = PageForm()
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+
+        if form.is_valid():
+            if category:
+                page = form.save(commit=False)
+                page.category = category
+                page.views = 0
+                page.save()
+
+                return redirect(reverse('rango:show_category', kwargs={'category_name_slug':category_name_slug}))
+        else:
+            print(form.errors)
+    context_dict = {'form': form, 'category': category}
+    return render(request, 'rango/add_page.html', context=context_dict) 
